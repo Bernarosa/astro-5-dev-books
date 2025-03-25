84b02d004629321f48fcb55b3246369f605c1708
Roberto Berna Larrosa <rbernalarrosa@MacBook-Pro-de-Roberto-2.local>
2025-03-25 16:13:32 +0100

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import typography from '@tailwindcss/typography'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography],
+}
+
